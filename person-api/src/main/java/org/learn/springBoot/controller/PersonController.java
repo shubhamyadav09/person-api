bd131d307b0bb7203aa66964613a9fd16cce0087
Shubham Yadav <shubham.y@turtlemint.com>
2020-06-27 23:59:14 +0530

    added endpoints

--- person-api/src/main/java/org/learn/springBoot/controller/PersonController.java
+++ person-api/src/main/java/org/learn/springBoot/controller/PersonController.java
@@ -1,7 +1,14 @@
 package org.learn.springBoot.controller;
 
+import java.util.List;
+
+import org.learn.springBoot.model.Person;
 import org.learn.springBoot.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,4 +30,25 @@ public class PersonController {
 		return "Hello Person";
 	}
 
+	@RequestMapping(method=RequestMethod.PUT, value="/update/{firstName}")
+	public ResponseEntity<HttpStatus> addPerson(@RequestBody Person person, @PathVariable String firstName) {
+		personServ.updatePerson(firstName, person);
+		
+		return ResponseEntity.ok(HttpStatus.OK);
+	}
+	
+	@RequestMapping(method=RequestMethod.GET, value="/getAll")
+	public List<Person> getAllPersons() {
+		return personServ.getAll();
+	}
+	
+	@RequestMapping(method=RequestMethod.GET, value="/get")
+	public List<Person> getPersons(@RequestParam String FirstName) {
+		return personServ.getPersons(FirstName);
+	}
+	
+	@RequestMapping(method=RequestMethod.DELETE, value="/delete")
+	public void deletePerson(@RequestParam String FirstName) {
+		personServ.deletePerson(FirstName);
+	}
 }
--- person-api/src/main/java/org/learn/springBoot/service/PersonService.java
+++ person-api/src/main/java/org/learn/springBoot/service/PersonService.java
@@ -1,8 +1,13 @@
 package org.learn.springBoot.service;
 
+import java.util.List;
+
 import org.learn.springBoot.model.Person;
 import org.learn.springBoot.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,8 +16,34 @@ public class PersonService {
 	@Autowired
 	private PersonRepository personRepo;
 	
+	@Autowired
+	private MongoTemplate mongoTemplate;
 	public Person addPerson(String fName, String LName) {
 		return personRepo.save(new Person(fName, LName));
 	}
 
+	public void updatePerson(String firstName, Person person) {
+		Query q = new Query();
+		q.addCriteria(Criteria.where("FirstName").is(firstName));		
+		mongoTemplate.findAndReplace(q, person);
+	}
+
+	public List<Person> getAll() {
+		return personRepo.findAll();
+		
+	}
+
+	public List<Person> getPersons(String firstName) {
+		Query q = new Query();
+		q.addCriteria(Criteria.where("FirstName").is(firstName));
+		
+		return mongoTemplate.find(q, Person.class);
+	}
+
+	public void deletePerson(String firstName) {
+		Query q = new Query();
+		q.addCriteria(Criteria.where("FirstName").is(firstName));
+		Person p = mongoTemplate.findOne(q, Person.class);
+		personRepo.delete(p);	
+	}
 }
